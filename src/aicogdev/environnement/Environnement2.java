abe4ab79512cb12fb304e4a0a2a9a6e83d6bac66
Julian Bruyat <bruyat.ju@live.fr>
2019-10-28 13:55:32 +0100

    fix environnement 2

--- src/aicogdev/environnement/Environnement2.java
+++ src/aicogdev/environnement/Environnement2.java
@@ -6,6 +6,6 @@ import aicogdev.interaction.Reaction;
 public class Environnement2 implements Environnement {
 	@Override
 	public Reaction agir(Action action) {
-		return new Reaction(action.numero - 1);
+		return new Reaction(action.numero == 2 ? 1 : 2);
 	}
 }
